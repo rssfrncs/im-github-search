b4de21a8b9bf1ac6920935505e03a6a332260993
rssfrncs <rssfrncs@gmail.com>
2019-11-27 21:46:42 +0000

    Updates

---
+++ precache-manifest.dc9c43699aa9a2ebaa13e59bc90c036d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6214c888406ca08ca0bbc46f446c9fd2",
+    "url": "/im-github-search/index.html"
+  },
+  {
+    "revision": "edd1b3be1fd3ac8dafac",
+    "url": "/im-github-search/static/css/main.c9cd4303.chunk.css"
+  },
+  {
+    "revision": "0b6d7dbd8e3c654b81ec",
+    "url": "/im-github-search/static/js/2.be41f8b3.chunk.js"
+  },
+  {
+    "revision": "edd1b3be1fd3ac8dafac",
+    "url": "/im-github-search/static/js/main.a9f8d6f1.chunk.js"
+  },
+  {
+    "revision": "a006c99ba902708b0b1b",
+    "url": "/im-github-search/static/js/runtime-main.20f543fa.js"
+  }
+]);
